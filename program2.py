e5da7513bb1672a5966d58ca89bf43270e4f18fb
FeketeBalage <153723762+FeketeBalage@users.noreply.github.com>
2025-01-22 13:23:11 +0100

    Add files via upload

---
+++ program2.py
@@ -0,0 +1,9 @@
+import random
+
+
+gyakorlatok = {
+    "kar": ["bicepsz hajlítás", "tricepsz nyújtás", "karhúzás", "fekvőtámasz"],
+    "láb": ["guggolás", "felhúzás", "kitörés", "lábnyújtás"],
+    "has": ["plank", "felülés", "lábemelés", "biciklizés"],
+    "mell": ["fekvőtámasz", "pados fekvenyomás", "karnyújtás", "csigás tárogatás"]
+}
